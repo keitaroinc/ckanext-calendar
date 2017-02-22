f32524a3af1eecd6011ff94c74e8559a871145d8
Petar Efnushev <petar.efnushev@keitaro.com>
2017-02-22 12:51:37 +0100

    Implement IActions, Iconfigurable and IAuthFunctions interfaces

--- ckanext/calendar/plugin.py
+++ ckanext/calendar/plugin.py
@@ -1,13 +1,46 @@
+import logging
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.calendar.model import setup as model_setup
+import ckanext.calendar.helpers as _h
+import ckanext.calendar.logic.auth as pauth
+
+log = logging.getLogger(__name__)
+
 
 class CalendarPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IActions)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.IAuthFunctions)
 
-    # IConfigurer
+    startup = False
+
+    ## IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'calendar')
+
+    def get_auth_functions(self):
+        return {'event_create': pauth.event_create}
+
+    ## IConfigurable
+
+    def configure(self, config):
+        self.startup = True
+
+        # Setup calendar model
+        model_setup()
+
+        self.startup = False
+
+    ## IActions
+
+    def get_actions(self):
+        module_root = 'ckanext.calendar.logic.action'
+        action_functions = _h._scan_functions(module_root)
+        return action_functions
