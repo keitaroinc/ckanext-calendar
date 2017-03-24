d3226f88678d49510c77af18ae686060aba597f6
Petar Efnushev <petar.efnushev@keitaro.com>
2017-03-24 10:39:21 +0100

    Refactor calendar controller

--- ckanext/calendar/controllers/calendar.py
+++ ckanext/calendar/controllers/calendar.py
@@ -1,10 +1,19 @@
 import json
+import logging
 
 from ckan.lib import base
 from ckan.plugins import toolkit
 from ckan import model, logic
-from ckan.common import c, _
+from ckan.common import c, _, request
+import ckan.lib.helpers as h
+import ckan.plugins as p
 
+
+log = logging.getLogger(__name__)
+redirect = base.redirect
+
+# TODO: Load limit from config var
+LIMIT = 5
 
 def _get_context():
     return {
@@ -21,16 +30,27 @@ def _get_action(action, data_dict):
 
 class CalendarController(base.BaseController):
 
-    def event_index(self):
-        events = _get_action('event_list', {'limit': 100})
+    def __get_page_number(self, params):
+        if p.toolkit.check_ckan_version(min_version='2.5.0', max_version='2.5.10'):
+            return self._get_page_number(params)
+        return h.get_page_number(params)
 
+    def event_index(self):
+        # TODO: Handle errors
+        page = self.__get_page_number(request.params)
+        c.page = page
+        c.limit = LIMIT
+        offset = (page - 1) * LIMIT if page > 1 else 0
+        data = _get_action('event_list', {'limit': LIMIT, 'offset': offset })
+        c.total = data['count']
         extra_vars = {
-            'events': events
+            'events': data['events']
         }
 
         return toolkit.render('events/events_list.html', extra_vars)
 
     def event_show(self, id):
+        # TODO: Handle errors
         try:
             event = _get_action('event_show', {'id': id})
         except (logic.ValidationError, logic.NotFound):
@@ -39,15 +59,48 @@ class CalendarController(base.BaseController):
         extra_vars = {
             'event': event
         }
-
         return toolkit.render('events/event_page.html', extra_vars)
 
-    def event_create(self):
+    def event_create(self, data=None):
+        # TODO: Handle errors
+        if request.method.lower() == 'post':
+            fields = {
+                'title': request.POST['event-name'],
+                'description': request.POST['event-description'],
+                'start': request.POST['add-event-start-date-input'],
+                'end': request.POST['add-event-end-date-input'],
+                'venue': request.POST['event-venue']
+            }
+            _get_action('event_create', fields)
+            h.flash_success(_('The event was created successfully.'))
+            redirect(h.url_for('event_index'))
 
-        # TODO implement functionality
+        return toolkit.render('events/event_form.html', extra_vars={})
 
+    def event_update(self, id):
+        # TODO: Handle errors
+        event = _get_action('event_show', {'id': id})
         extra_vars = {
-            'events': []
+            'event': event
         }
+        if request.method.lower() == 'post':
+            fields = {
+                'title': request.POST['event-name'],
+                'description': request.POST['event-description'],
+                'start': request.POST['add-event-start-date-input'],
+                'end': request.POST['add-event-end-date-input'],
+                'venue': request.POST['event-venue'],
+                'id':id
+            }
+            _get_action('event_update', fields)
+            h.flash_success(_('The event was updated successfully.'))
+            redirect(h.url_for('event_show', id=id))
 
-        return toolkit.render('events/events_list.html', extra_vars)
+        return toolkit.render('events/event_update.html', extra_vars)
+
+    def event_delete(self, id):
+        # TODO: Handle errors
+        _get_action('event_delete', {'id': id})
+        h.flash_success(_('The event was removed successfully.'))
+        redirect(h.url_for('event_index'))
+        
